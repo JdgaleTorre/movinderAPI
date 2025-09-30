bf2eda14daa71934f281b4b3e4f4a82859d27f05
Jose Gale <josedavidgale@gmail.com>
2025-09-30 13:29:12 +0100

    Test

--- recommendations/apps.py
+++ recommendations/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class RecommendationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recommendations'
+    label = 'recommendations_app'  # custom label to avoid conflicts
     
     def ready(self):
         from . import mlModel
